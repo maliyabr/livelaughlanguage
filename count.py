8d9d15fedfc35aec6c994090638ae7a4fcbe9abd
shebaelena9 <alexis_giff@brown.edu>
2021-04-17 13:15:55 -0400

    count, add user input

--- count.py
+++ count.py
@@ -1,6 +1,11 @@
 # define mic output as string
-string = "hello how are you hello"
-substring = "hello"
+string = "hello how are you hello no question"
+substring = input("Please enter a keyword\n")
 count = string.count(substring)
 #print count
-print("The count is:", count)
+print("The count for " + substring + " is:", count)
+
+
+
+
+
